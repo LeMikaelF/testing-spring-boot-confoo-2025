a2e115a83c5cb240492d28bdfe3c56d104de0868
Mikaël Francoeur <mikael.francoeur@ticketmaster.com>
2025-02-13 21:39:03 -0500

    json wip

--- src/main/java/com/mikaelfrancoeur/testingspringboot/json/Relationship.java
+++ src/main/java/com/mikaelfrancoeur/testingspringboot/json/Relationship.java
@@ -1,7 +1,73 @@
 package com.mikaelfrancoeur.testingspringboot.json;
 
-import java.time.LocalDate;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonValue;
 
-record Relationship(String sourceId, String destinationId, LocalDate createdDate) {
+import jakarta.validation.constraints.Pattern;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
+@EqualsAndHashCode
+@RequiredArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
+class Relationship {
+    @Getter
+    private final String sourceId;
+    @Getter
+    private final String destinationId;
+    private final String typeSuffix;
+    @JsonIgnore
+    private final Type type;
+
+    @JsonCreator
+    static Relationship fromJson(
+            @Pattern(regexp = "\\w+-\\w+")
+            String tuple,
+            @Pattern(regexp = "(int|ext-.*)")
+            @JsonProperty("relationshipType")
+            String typeString
+    ) {
+        String sourceId = tuple.substring(0, tuple.indexOf("-"));
+        String destinationId = tuple.substring(tuple.indexOf("-") + 1);
+        Type type = typeString.startsWith("int-") ? Type.INTERNAL : Type.EXTERNAL;
+        String typeSuffix = typeString.substring("int-".length());
+
+        return new Relationship(sourceId, destinationId, typeSuffix, type);
+    }
+
+    @JsonValue
+    private Object jsonValue() {
+        return switch (type) {
+            case INTERNAL -> serializeInternal();
+            case EXTERNAL -> serializeExternal();
+        };
+    }
+
+    private Object serializeInternal() {
+        class RelationshipDTO extends Relationship {
+            public RelationshipDTO(String sourceId, String destinationId, String typeSuffix, Type type) {
+                super(sourceId, destinationId, typeSuffix, type);
+            }
+
+            @JsonValue(false)
+            private Void jsonValue() {
+                return null;
+            }
+        }
+        return new RelationshipDTO(sourceId, destinationId, typeSuffix, type);
+    }
+
+    private Object serializeExternal() {
+        return "allo";
+    }
+
+    private enum Type {
+        INTERNAL, EXTERNAL;
+    }
 }
---
+++ src/test/java/com/mikaelfrancoeur/testingspringboot/json/RelationshipJsonTest.java
@@ -0,0 +1,67 @@
+package com.mikaelfrancoeur.testingspringboot.json;
+
+import java.io.IOException;
+import java.io.StringReader;
+
+import org.json.JSONException;
+import org.junit.jupiter.api.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.json.JsonContent;
+import org.springframework.boot.test.json.ObjectContent;
+
+@JsonTest
+class RelationshipJsonTest {
+
+    @Autowired
+    private JacksonTester<Relationship> jacksonTester;
+
+    @Test
+    void internalRoundTrip() throws IOException, JSONException {
+        // language=JSON
+        String json = """
+                {
+                  "tuple": "123-456",
+                  "relationshipType": "int-friendship"
+                }
+                """;
+        ObjectContent<Relationship> objectContent = jacksonTester.read(new StringReader(json));
+        Relationship relationship = objectContent.getObject();
+
+        JsonContent<Relationship> jsonContent = jacksonTester.write(relationship);
+        // language=JSON
+        String expected = """
+                {
+                  "sourceId": "123",
+                  "destinationId": "456",
+                  "typeSuffix": "friendship"
+                }
+                """;
+        JSONAssert.assertEquals(expected, jsonContent.getJson(), true);
+    }
+
+    @Test
+    void externalRoundTrip() throws JSONException, IOException {
+        // language=JSON
+        String json = """
+                {
+                  "tuple": "123-456",
+                  "relationshipType": "ext-friendship"
+                }
+                """;
+        ObjectContent<Relationship> objectContent = jacksonTester.read(new StringReader(json));
+        Relationship relationship = objectContent.getObject();
+
+        JsonContent<Relationship> jsonContent = jacksonTester.write(relationship);
+        // language=JSON
+        String expected = """
+                {
+                  "sourceId": "1xx",
+                  "destinationId": "4xx"
+                }
+                """;
+        JSONAssert.assertEquals(expected, jsonContent.getJson(), true);
+    }
+}
