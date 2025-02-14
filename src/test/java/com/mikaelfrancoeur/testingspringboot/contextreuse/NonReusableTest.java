7ecc4651aa6b18a79ad3465e3d172c530dbdc4ef
Mikaël Francoeur <mikael.francoeur@ticketmaster.com>
2025-02-13 19:31:31 -0500

    context reuse

---
+++ src/test/java/com/mikaelfrancoeur/testingspringboot/contextreuse/NonReusableTest.java
@@ -0,0 +1,23 @@
+package com.mikaelfrancoeur.testingspringboot.contextreuse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+@EmbeddedKafka
+@SpringBootTest
+class NonReusableTest {
+
+    @MockitoBean
+    private MyBean myBean;
+
+    @Test
+    void test() {
+        System.out.println("This is the non-reusable test");
+    }
+
+    static class MyBean {
+
+    }
+}
