989bb8695d989f9f1f8617a3a3aa40b12e17b8b6
Mikaël Francoeur <mikael.francoeur@ticketmaster.com>
2025-02-14 19:43:54 -0500

    add missing annotation

--- src/main/java/com/mikaelfrancoeur/testingspringboot/demos/contextcaching/ContextCachingConfig.java
+++ src/main/java/com/mikaelfrancoeur/testingspringboot/demos/contextcaching/ContextCachingConfig.java
@@ -1,7 +1,9 @@
 package com.mikaelfrancoeur.testingspringboot.demos.contextcaching;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@Configuration
 @EnableScheduling
 class ContextCachingConfig {
 }
